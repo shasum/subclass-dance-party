c7c4d70ecacc15ea4f0c19f284ccb1dc3b2d08d1
Sanjay Chakrapani <csanjay@gmail.com>
2015-08-25 11:36:21 -0400

    passed in time to setTimeout

--- src/dancer.js
+++ src/dancer.js
@@ -14,8 +14,8 @@ var makeDancer = function(top, left, timeBetweenSteps){
 makeDancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  //
-  setTimeout(this.step.bind(this), 100);
+  // this.step()
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 makeDancer.prototype.setPosition = function(top, left){
